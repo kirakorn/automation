b7f31e54a986bb07c278e47d261c5f6d03880144
Kirakorn Chimkool <kirakorn@gmail.com>
2018-08-02 09:02:02 +0700

    Create install_brew.sh

---
+++ install_brew.sh
@@ -0,0 +1,11 @@
+# Install Homebrew if we don't have it
+if test ! $(which brew); then
+  echo "Installing homebrew"
+  ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+fi
+
+# Update homebrew recipes
+brew update
+
+# Cleanup
+brew cleanup
